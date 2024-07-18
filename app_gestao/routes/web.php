64e0749255120e227e502c33a7f498dfaa932a0a
Fertsuri <fsab95@gmail.com>
2024-07-18 10:54:06 -0300

    add

--- app_gestao/routes/web.php
+++ app_gestao/routes/web.php
@@ -4,8 +4,7 @@ use App\Http\Controllers\ContatoController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\SobrenosController;
-
-
+use Faker\Guesser\Name;
 
 // Route::get(
 //     '/contato/{nome}/{categoria_id}',
@@ -27,23 +26,38 @@ o parametro, nesse caso sendo "principal"
 
 */
 // Route::get('/', "PrincipalController@principal");
-Route::get('/', [PrincipalController::class, 'principal']);
-Route::get('/contato', [ContatoController::class, 'contato']);
-Route::get('/sobrenos', [SobrenosController::class, 'Sobrenos']);
+Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
+Route::get('/sobrenos', [SobrenosController::class, 'Sobrenos'])->name('site.sobrenos');
 Route::get('/login', function () {
     return 'Login';
-});
+})->name('site.login');
 
 
 //prefix serve para agrupar varias rotas definindo um caminho maior por exemplo, "app/clientes"
 route::prefix('/app')->group(function () {
     Route::get('/clientes', function () {
         return 'Clientes';
-    });
+    })->name('app.clientes');
     Route::get('/fornecedores', function () {
         return 'Fornecedores';
-    });
+    })->name('app.fornecedores');
     Route::get('/produtos', function () {
         return 'Produtos';
-    });
+    })->name('app.produtos');
 });
+
+Route::get('/rota1', function() {
+ echo "Rota1";
+})->name('site.rota1');
+
+
+//outra forma de usar a função redirect, dentro da função de callback da rota
+
+route::get('/rota2', function(){
+    return redirect()->route('site.rota1');
+})->name('site.rota2');
+
+
+//uma forma para usar a função redirect toda vez que cair na rota de origem, será redirecionado para a segunda rota
+Route::redirect('/rota2', '/rota1');

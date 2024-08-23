36f4f33aefd04e6a97c142fecd4c43fbe9ad3aa4
aerlonga <aerlon.as@gmail.com>
2024-08-22 22:33:32 -0300

    add

--- app_gestao/start.sh
+++ app_gestao/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker-compose up -d       # Isso vai iniciar os serviços do Docker em segundo plano (detached mode).
+# docker-compose up -d       # Isso vai iniciar os serviços do Docker em segundo plano (detached mode).
 docker-compose exec app bash  # Isso vai abrir uma sessão Bash dentro do contêiner "app".
